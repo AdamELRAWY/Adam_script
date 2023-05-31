8731c1ae31076d4551fd5cb9fba5bc47c20a4349
AdamELRAWY <134511742+AdamELRAWY@users.noreply.github.com>
2023-05-31 09:36:10 +0200

    Update Attaque_de_force_brute_de_repertoires.sh

--- Attaque_de_force_brute_de_repertoires.sh
+++ Attaque_de_force_brute_de_repertoires.sh
@@ -3,9 +3,13 @@
 # Prompt the user to enter the target URL
 read -p "Enter the target URL: " url
 
+# Prompt the user to enter the name of the output file
+read -p "Enter the name of the output file (without extension): " output_filename
+
 # Set the wordlist and output file
 wordlist="/usr/share/dirb/wordlists/common.txt"
-output_file="dirb-results.txt"
+output_file="$output_filename.txt"
+pdf_output_file="$output_filename.pdf"
 
 # Check if DirB and DirBuster are installed
 if ! [ -x "$(command -v dirb)" ]; then
@@ -18,5 +22,10 @@ fi
 echo "Running directory bruteforce attack with DirB..."
 dirb "$url" "$wordlist" >> "$output_file"
 
+# Convert the output file to PDF using Pandoc
+pandoc "$output_file" -o "$pdf_output_file"
 
-echo "Directory bruteforce attacks complete! Results saved to $output_file."
+# Remove the intermediate text file
+rm "$output_file"
+
+echo "Directory bruteforce attacks complete! Results saved to $pdf_output_file."
